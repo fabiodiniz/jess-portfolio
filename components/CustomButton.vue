9549de34d3e02e518bef8fddc48fe52bce7b7948
Fábio Diniz <contato@fabiodiniz.com.br>
2021-06-19 22:43:45 -0300

    ✨ Adiciona botão e input padrão

---
+++ components/CustomButton.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  button.text-xs.bg-gray-200.text-gray-600.border.border-gray-200.rounded-md.py-2.px-5.font-bold.uppercase(
+    :type="type"
+  )
+    slot(name="default")
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    type: {
+      type: String,
+      default: 'submit'
+    }
+  }
+})
+</script>
+
+<style scoped>
+button:hover {
+  opacity: 0.9;
+}
+</style>
---
+++ components/CustomInput.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  input.text-sm.text-black.placeholder-gray-500.border.border-gray-200.rounded-md.p-2(
+    type="text"
+    :value="value"
+    :placeholder="placeholder"
+    @input="$emit('input', value)"
+    class="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none"
+  )
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
