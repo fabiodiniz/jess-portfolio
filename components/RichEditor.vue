0399858ed89e912a819c7c7430559e6b46af42b3
Fábio Diniz <contato@fabiodiniz.com.br>
2021-06-23 10:53:49 -0300

    ➕ Configura CKEditor 5

---
+++ components/RichEditor.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .rich-editor
+    ckeditor(
+      :editor="editor"
+      :value="value"
+      :config="editorConfig"
+      @input="ev => $emit('input', ev)"
+    )
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import CKEditor from '@ckeditor/ckeditor5-vue2'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+@Component({
+  components: {
+    ckeditor: CKEditor.component,
+  },
+  setup (props) {
+    const editorConfig = props.options
+
+    return { editorConfig, editor: ClassicEditor }
+  },
+})
+export default class RichEditor extends Vue {
+  @Prop(String) value: string | undefined
+  @Prop({ default: () => ({}) }) options: object | undefined
+}
+</script>
---
+++ plugins/rich-editor.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import RichEditor from '@/components/RichEditor'
+
+Vue.component('RichEditor', RichEditor)
